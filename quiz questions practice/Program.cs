024f14fd17d46d1729f480df34d9a8dc9f37b697
hassan <hassan26khan@gmail.com>
2016-03-16 22:46:37 +0500

    2nd Commit

--- quiz questions practice/Program.cs
+++ quiz questions practice/Program.cs
@@ -23,6 +23,23 @@ namespace quiz_questions_practice
             Console.WriteLine("NUM 2 : "+ num2);
             Console.ReadLine();
         }//Swap function end
+        public static void swapref(ref int a,ref int b)//Start Swapping function with Reference
+        {
+            a= 20;
+            b = 30;
+            Console.WriteLine("Before Swapping Values are");
+            Console.WriteLine("Value of num1 : " + a);
+            Console.WriteLine("Value of num2 : " + b);
+            Console.WriteLine("APPLYING SWAPPING");
+            a = a + b;
+            b= a - b;
+           a = a - b;
+           Console.WriteLine("After Swapping Values are ");
+           Console.WriteLine("Value of num1 After Swappimg : " + a);
+           Console.WriteLine("Value of num2 After Swapping : " + b);
+           // Console.ReadLine();
+           
+        }//End of swapping fun
         public int Find( int []a,int b)//Start of find function
         {
           // b = 2;
@@ -38,11 +55,19 @@ namespace quiz_questions_practice
         }//End of find functon
         static void Main(string[] args)
         {
+            int x = 30, y = 30;
             Program obj = new Program();
+
             int[] arra =new int[] { 1, 2, 3 };
+            //Simple swap function
             obj.Swap();
+            //Find the value in Array
             int ans=obj.Find(arra,3);
             Console.WriteLine("Ans is: {0} ", ans);
+            //Swapping function call With ref
+           swapref(ref x,ref y);
+           Console.WriteLine("Value With Reference "+  x);
+           Console.WriteLine("second value with Reference "+ y);
             Console.ReadLine();
         }
     }
